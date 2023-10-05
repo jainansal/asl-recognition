cdbac91f1c8eae3354e1d47c8f1fd0ab58d6e6dc
Ansal Jain <jainansal@gmail.com>
2023-10-05 16:48:29 +0530

    ft: add model

--- test_classifier.py
+++ test_classifier.py
@@ -1,7 +1,11 @@
+import numpy as np
+import pickle
+
 import cv2
 import mediapipe as mp
 
-cap = cv2.VideoCapture(0)
+model_dict = pickle.load(open('model.p', 'rb')) 
+model = model_dict['model']
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -9,6 +13,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+cap = cv2.VideoCapture(0)
 while True:
   _, frame = cap.read()
 
@@ -17,6 +22,7 @@ while True:
   results = hands.process(frame_rgb)
 
   if results.multi_hand_landmarks:
+    data_aux = []
     for hand_landmarks in results.multi_hand_landmarks:
 
         # display landmarks
@@ -28,5 +34,13 @@ while True:
           mp_drawing_styles.get_default_hand_connections_style() # style
         )
 
+    for i in range(len(hand_landmarks.landmark)):
+        x = hand_landmarks.landmark[i].x
+        y = hand_landmarks.landmark[i].y
+        data_aux.append(x)
+        data_aux.append(y)
+
+    model.predict([np.asarray(data_aux)])
+
   cv2.imshow('frame', frame)
-  cv2.waitKey(25)
+  cv2.waitKey(1)
