16af98849876d860bc9471f929be810ff40cc1f2
Ansal Jain <jainansal@gmail.com>
2023-10-05 16:08:36 +0530

    ft: data, labels

--- create_dataset.py
+++ create_dataset.py
@@ -2,7 +2,6 @@ import os
 
 import mediapipe as mp
 import cv2
-import matplotlib.pyplot as plt
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -12,8 +11,12 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
+data = []
+labels = []
 for dir_ in os.listdir(DATA_DIR):
-  for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+  print('...processing', os.path.join(DATA_DIR, dir_), '...')
+  for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+    data_aux = []
     img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
@@ -21,15 +24,21 @@ for dir_ in os.listdir(DATA_DIR):
 
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-          img_rgb, # img to draw
-          hand_landmarks, # model output
-          mp_hands.HAND_CONNECTIONS, # hand connections
-          mp_drawing_styles.get_default_hand_landmarks_style(), # style
-          mp_drawing_styles.get_default_hand_connections_style() # style
-        )
 
-    plt.figure()
-    plt.imshow(img_rgb)
+        # display landmarks
+        # mp_drawing.draw_landmarks(
+        #   img_rgb, # img to draw
+        #   hand_landmarks, # model output
+        #   mp_hands.HAND_CONNECTIONS, # hand connections
+        #   mp_drawing_styles.get_default_hand_landmarks_style(), # style
+        #   mp_drawing_styles.get_default_hand_connections_style() # style
+        # )
 
-plt.show()
+        for i in range(len(hand_landmarks.landmark)):
+          x = hand_landmarks.landmark[i].x
+          y = hand_landmarks.landmark[i].y
+          data_aux.append(x)
+          data_aux.append(y)
+      
+      data.append(data_aux)
+      labels.append(dir_)
