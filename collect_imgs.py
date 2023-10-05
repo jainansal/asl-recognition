8aa29189abb1d4d4d1155c2333964e9070a485f9
Ansal Jain <jainansal@gmail.com>
2023-10-05 17:04:39 +0530

    ft: classifier complete

--- collect_imgs.py
+++ collect_imgs.py
@@ -12,24 +12,26 @@ cap = cv2.VideoCapture(0)
 for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
-    
+
     print('Collecting images for class {}'.format(j))
 
     done = False
     while True:
         _, frame = cap.read()
-        cv2.putText(frame, "Ready? Press 'Q' ! :)", (100,50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0,255,0), 3, cv2.LINE_AA)
+        cv2.putText(frame, "Ready? Press 'Q' ! :)", (100, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
         cv2.imshow("frame", frame)
 
         if cv2.waitKey(25) == ord('q'):
             break
-        
+
     counter = 0
     while counter < dataset_size:
         _, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+        cv2.imwrite(os.path.join(DATA_DIR, str(
+            j), '{}.jpg'.format(counter)), frame)
 
         counter += 1
 
--- test_classifier.py
+++ test_classifier.py
@@ -4,7 +4,7 @@ import pickle
 import cv2
 import mediapipe as mp
 
-model_dict = pickle.load(open('model.p', 'rb')) 
+model_dict = pickle.load(open('model.p', 'rb'))
 model = model_dict['model']
 
 mp_hands = mp.solutions.hands
@@ -14,33 +14,56 @@ mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 cap = cv2.VideoCapture(0)
+
+labels_dict = {0: 'A', 1: 'B', 2: 'L'}
 while True:
-  _, frame = cap.read()
 
-  frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-  results = hands.process(frame_rgb)
-
-  if results.multi_hand_landmarks:
     data_aux = []
-    for hand_landmarks in results.multi_hand_landmarks:
+    x_ = []
+    y_ = []
 
-        # display landmarks
-        mp_drawing.draw_landmarks(
-          frame, # img to draw
-          hand_landmarks, # model output
-          mp_hands.HAND_CONNECTIONS, # hand connections
-          mp_drawing_styles.get_default_hand_landmarks_style(), # style
-          mp_drawing_styles.get_default_hand_connections_style() # style
-        )
+    _, frame = cap.read()
 
-    for i in range(len(hand_landmarks.landmark)):
-        x = hand_landmarks.landmark[i].x
-        y = hand_landmarks.landmark[i].y
-        data_aux.append(x)
-        data_aux.append(y)
+    H, W, _ = frame.shape
 
-    model.predict([np.asarray(data_aux)])
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-  cv2.imshow('frame', frame)
-  cv2.waitKey(1)
+    results = hands.process(frame_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+
+            # display landmarks
+            mp_drawing.draw_landmarks(
+                frame,  # img to draw
+                hand_landmarks,  # model output
+                mp_hands.HAND_CONNECTIONS,  # hand connections
+                mp_drawing_styles.get_default_hand_landmarks_style(),  # style
+                mp_drawing_styles.get_default_hand_connections_style()  # style
+            )
+
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
+            data_aux.append(x)
+            data_aux.append(y)
+            x_.append(x)
+            y_.append(y)
+
+        x1 = int(min(x_) * W) - 10
+        y1 = int(min(y_) * H) - 10
+        x2 = int(max(x_) * W) - 10
+        y2 = int(max(y_) * H) - 10
+
+        prediction = model.predict([np.asarray(data_aux)])
+
+        predicted_character = labels_dict[int(prediction[0])]
+
+        # print(predicted_character)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
+        cv2.putText(frame, predicted_character, (x1, y1-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
